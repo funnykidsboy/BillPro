06caa7cd2e8fbe37f44f82dd39e71c7f3e27628b
Khanh Dnag <youremail@example.com>
2025-08-10 04:13:29 +0700

    Add Hello World iOS15 project

--- BillPro/BillProApp.swift
+++ BillPro/BillProApp.swift
@@ -4,7 +4,8 @@ import SwiftUI
 struct BillProApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            Text("Hello iOS 15")
+                .padding()
         }
     }
 }
