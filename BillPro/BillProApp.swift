8a783174eb5e048d4f0f737a471b92661435fdc9
Khanh Dnag <youremail@example.com>
2025-08-10 04:21:35 +0700

    Add fixed Hello World iOS15 project

---
+++ BillPro/BillProApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct BillProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello iOS 15")
+                .padding()
+        }
+    }
+}
